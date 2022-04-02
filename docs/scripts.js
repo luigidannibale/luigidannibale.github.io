ee4a579bbce38e8c02711491bd1ab089f5b236a5
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-04-02 13:59:55 +0200

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,11 +1,14 @@
 document.getElementById ("translate").addEventListener ("click", translate);    
 
-function translate(){                      
+function translate(){         
+    alert(window.location.pathname)             
     if(window.location.pathname.startsWith("pages/english")){
+        alert("ciao")
         //to ita 
         window.location.href = "luigidannibale.github.io/" + window.location.hash
     }
     else{
+        alert("hello")
         //to eng
         window.location.href = "luigidannibale.github.io/pages/english/" + window.location.hash
     }    
