90de85d603912df3981502d101d39755ea3212b9
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-04-02 15:47:14 +0200

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,5 +1,10 @@
 document.getElementById ("translate").addEventListener ("click", translate);    
+window.addEventListener("resize",onResize);
 
 function translate(){             
     window.location.href = "https://luigidannibale.github.io/pages/english/" + window.location.hash;      
 }
+function onResize(){
+    window.location.href = "https://luigidannibale.github.io/" + window.location.hash;  
+}
+
