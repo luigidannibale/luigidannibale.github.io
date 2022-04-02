174242fd6d4732214c73016911b90d23f85f379d
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-04-02 15:53:09 +0200

    Update scripts.js

--- pages/english/docs/scripts.js
+++ pages/english/docs/scripts.js
@@ -1,5 +1,9 @@
 document.getElementById ("translate").addEventListener ("click", translate);    
+window.addEventListener("resize",onResize);
 
 function translate(){         
     window.location.href = "https://luigidannibale.github.io/" + window.location.hash;    
+}
+function onResize(){
+    window.location.href = "https://luigidannibale.github.io/" + window.location.hash;  
 }
